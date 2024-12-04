35cae08eb79db46d905f8649d64713244d542bb2
Jan Neumann <janneu@gmx.net>
2024-12-04 15:18:44 +0100

    add categories screen

---
+++ lib/screens/meals.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final class MealsScreen extends StatelessWidget {
+  const MealsScreen( { super.key })
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
